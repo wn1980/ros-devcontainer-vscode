2b28c0d3ff97858cb5c0ff8fa61d4bd9b778a7f8
Your Name <you@example.com>
2021-05-28 14:34:03 +0700

    update to portainer 2.5.1

--- portainer.sh
+++ portainer.sh
@@ -11,4 +11,4 @@ docker run -d -p 8000:8000 -p 9000:9000 \
     --restart=always \
     -v /var/run/docker.sock:/var/run/docker.sock \
     -v portainer_data:/data \
-    portainer/portainer-ce:2.5.0
+    portainer/portainer-ce:2.5.1
